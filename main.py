c4dd48fff2d7a250e57d4ed101a6f4cfcf513d7d
Paulomascaro <mascaropr@gmail.com>
2024-08-28 22:33:19 -0300

    Feat: Consultas diaria, semanal, mensal e anual

--- main.py
+++ main.py
@@ -5,9 +5,6 @@ import pandas as pd
 # Definir o fuso horário de Brasília
 brasilia_tz = pytz.timezone('Etc/GMT-3')
 
-# Configurar a localização do cache para o fuso horário de Brasília
-yf.set_tz_cache_location(brasilia_tz)
-
 # Exemplo: Obter dados do Banco Santander
 santander = yf.Ticker("SAN.MC")
 hist = santander.history(period="5y")
@@ -17,17 +14,63 @@ hist.index = hist.index.tz_convert(brasilia_tz)
 
 # Definir os timestamps iniciais e finais no fuso horário de Brasília
 date_time_inicial = pd.Timestamp('2020-01-06', tz=brasilia_tz)
-date_time_final = pd.Timestamp('2020-01-10T12', tz=brasilia_tz)
+date_time_final = pd.Timestamp('2020-12-31T12', tz=brasilia_tz)
 
 # Iterar sobre o dataframe
 for index, row in hist.iterrows():
     if date_time_inicial < index < date_time_final:
         print(row)
 
+# Adicionar coluna de variação diária
+hist['Daily Change (%)'] = (hist['Close'] - hist['Open']) / hist['Open'] * 100
+
+# Exibir análise diária para um intervalo específico
+daily_analysis = hist.loc[date_time_inicial:date_time_final]
+print(daily_analysis[['Open', 'Close', 'Daily Change (%)', 'Volume']])
 
 
-  # hist[date_time_inicial].open
-# hist[date_time_final].close
-# hist[
+# Resample para semanal e calcular variações e volumes
+weekly_analysis = hist.resample('W').agg({
+    'Open': 'first',
+    'Close': 'last',
+    'High': 'max',
+    'Low': 'min',
+    'Volume': 'sum'
+})
+
+# Calcular variação semanal
+weekly_analysis['Weekly Change (%)'] = (weekly_analysis['Close'] - weekly_analysis['Open']) / weekly_analysis['Open'] * 100
+
+print(weekly_analysis[['Open', 'Close', 'Weekly Change (%)', 'Volume']])
+
+
+# Resample para mensal e calcular variações e volumes
+monthly_analysis = hist.resample('M').agg({
+    'Open': 'first',
+    'Close': 'last',
+    'High': 'max',
+    'Low': 'min',
+    'Volume': 'sum'
+})
+
+# Calcular variação mensal
+monthly_analysis['Monthly Change (%)'] = (monthly_analysis['Close'] - monthly_analysis['Open']) / monthly_analysis['Open'] * 100
+print(monthly_analysis[['Open', 'Close', 'Monthly Change (%)', 'Volume']])
+
+
+# Resample para anual e calcular variações e volumes
+annual_analysis = hist.resample('A').agg({
+    'Open': 'first',
+    'Close': 'last',
+    'High': 'max',
+    'Low': 'min',
+    'Volume': 'sum'
+})
+
+# Calcular variação anual
+annual_analysis['Yearly Change (%)'] = (annual_analysis['Close'] - annual_analysis['Open']) / annual_analysis['Open'] * 100
+
+print(annual_analysis[['Open', 'Close', 'Yearly Change (%)', 'Volume']])
+
 
 
