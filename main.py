3698e8dc43d579c894eebcdbcae9e7591fb5bd1e
Paulomascaro <mascaropr@gmail.com>
2024-08-31 17:17:25 -0300

    Feat: Finalização

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import yfinance as yf
-import pytz
+import pytz  #trabalha com date
 import pandas as pd
 
 from analise_diaria import analise_diaria, validar_data
@@ -20,17 +20,17 @@ hist.index = hist.index.tz_convert(brasilia_tz)
 date_time_inicial = pd.Timestamp('2019-01-01', tz=brasilia_tz)
 date_time_final = pd.Timestamp('2024-08-30T12', tz=brasilia_tz)
 
-# Iterar sobre o dataframe
-for index, row in hist.iterrows():
-    if date_time_inicial < index < date_time_final:
-        print(row)
+# # Iterar sobre o dataframe
+# for index, row in hist.iterrows():
+#     if date_time_inicial < index < date_time_final:
+#         print(row)
 
 # Adicionar coluna de variação diária
 hist['Daily Change (%)'] = (hist['Close'] - hist['Open']) / hist['Open'] * 100
 
 # Exibir análise diária para um intervalo específico
 hist_diario = hist.loc[date_time_inicial:date_time_final]
-print(hist_diario[['Open', 'Close', 'Daily Change (%)', 'Volume']])
+# print(hist_diario[['Open', 'Close', 'Daily Change (%)', 'Volume']])
 
 
 # Resample para semanal e calcular variações e volumes
@@ -75,15 +75,18 @@ hist_anual = hist.resample('A').agg({
 hist_anual['Yearly Change (%)'] = (hist_anual['Close'] - hist_anual['Open']) / hist_anual['Open'] * 100
 # print(hist_anual[['Open', 'Close', 'Yearly Change (%)', 'Volume']])
 
-
+op = -1
 while op != 0:
-    print("####### MENU #######")
-    op = int(input("Digite a opção desejada: "
-             "1 - Analise Diaria"
-             "2 - Analise Semanal"
-             "3 - Analise Mensal"
-             "0 - Sair"))
-
+    print("####### MENU #######\n")
+    op = int(input("Digite a opção desejada: \n"
+             "1 - Analise Diaria: \n"
+             "2 - Analise Semanal: \n"
+             "3 - Analise Mensal: \n"
+             "0 - Sair\n"))
+    
+    if op == 0:
+        break
+    
     data_inicio = input('Digite a data de início (YYYY-MM-DD): ')
     data_fim = input('Digite a data de fim (YYYY-MM-DD): ')
     if not (validar_data(data_inicio) and validar_data(data_fim)):
@@ -95,13 +98,10 @@ while op != 0:
     if op == 1:
         analise_diaria(data_inicio, data_fim)
 
-    elif op == 2:
+    if op == 2:
         analise_semanal(data_inicio, data_fim)
 
-    elif op == 3:
+    if op == 3:
         analise_mensal(data_inicio, data_fim)
-
-    elif op == 0:
-        break
 
 
